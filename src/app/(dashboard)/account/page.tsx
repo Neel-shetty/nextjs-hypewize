19489f47f9ee8788f464290d8340372edb0bad25
Neel <neelnarayanshetty@gmail.com>
2023-07-01 05:08:00 +0530

    update

--- src/app/(dashboard)/account/page.tsx
+++ src/app/(dashboard)/account/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { FC } from "react";
 
@@ -13,6 +14,17 @@ const page: FC<pageProps> = ({}) => {
       <div className="hidden h-10"></div>
       <p className="text:sm text-[#7A7E8F]">Email</p>
       <Input />
+      <div className="w-full justify-end flex align-middle">
+        <div className="mb-5 gap-4 flex-row">
+          <Button variant={"secondary"} size={"lg"} className="bg-white">
+            Cancel
+          </Button>
+          {/* <div className="w-[10px]"></div> */}
+          <Button className="mt-5 bg-[#2745FA]" variant="default" size="lg">
+            Save
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
