8f9e2b7d040fcaf228d67f033e62dfede017f3b8
Neel <neelnarayanshetty@gmail.com>
2023-07-02 00:59:21 +0530

    landing page wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,64 +1,108 @@
+import BgVector from "@/components/bg-vector";
 import { MoveRight } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="bg-gray-50 ">
-      <div className="w-[90%] items-center flex flex-col justify-between ">
-        <div className="flex flex-row justify-between items-center p-5">
-          <div className="flex flex-row gap-5 justify-between w-full">
-            <div></div>
-            <div className="flex flex-row gap-5 justify-between">
-              <button className="border-[#2745FA] text-[#2745FA] px-5 py-2 border rounded-full">
-                Pricing
-              </button>
-
-              <button className="bg-[#2745FA] text-white px-5 py-2 rounded-full flex flex-row justify-center items-center gap-3">
-                Login
-                <MoveRight />
-              </button>
-            </div>
+    <div className="bg-gray-50 px-10 py-10 w-full min-h-screen">
+      <div className="flex flex-row items-end w-full gap-5 justify-end">
+        <button className="border-[#DEDEDE] text-[#2745FA] px-5 py-2 border rounded-full">
+          Pricing
+        </button>
+        <Link href={'/sign-in'} className="bg-[#2745FA] text-white px-5 py-2 rounded-full flex flex-row justify-center items-center gap-3">
+          Login
+          <MoveRight />
+        </Link>
+      </div>
+      <BgVector />
+      <div className="flex flex-row m-10">
+        <div className="flex-1">
+          <div className="w-full">
+            <span className="text-blue-600 text-4xl font-medium leading-10 tracking-widest">
+              GPT-Powered Live Chat
+            </span>
+            <span className="text-zinc-900 text-4xl font-medium leading-10 tracking-widest">
+              {" "}
+            </span>
+            <span className="text-black text-4xl font-medium leading-10 tracking-widest">
+              Your Gateway to Productive Communication
+            </span>
+          </div>
+          <div className="w-full text-neutral-500 text-lg font-normal leading-loose">
+            Talking to your customers should be easy.
+            {/* <br /> */}
+            Chat with them from the collaboration
+            {/* <br /> */}
+            tools you already use: Teams and Slack.
+          </div>
+          <div className="w-full text-neutral-500 text-lg font-normal leading-loose">
+            Build ChatGPT powered chatbots trained
+            {/* <br /> */}
+            on your website data. Easily handoff chats
+            {/* <br /> */}
+            between ChatGPT and human agents.
           </div>
         </div>
-        <div className="flex flex-row justify-between items-center">
-          <div className="flex-1">
-            {/* <p>GPT-Powered Live Chat</p>
-          <p>Your Gateway to Productive Communication</p> */}
-            <div className="w-[593px] h-44">
-              <span className="text-blue-600 text-[46px] font-medium leading-10 tracking-widest">
-                GPT-Powered Live Chat
-              </span>
-              <span className="text-zinc-900 text-[46px] font-medium leading-10 tracking-widest">
-                {" "}
-              </span>
-              <span className="text-black text-[42px] font-medium leading-10 tracking-widest">
-                Your Gateway to Productive Communication
-              </span>
-              <div className="w-[372.06px] h-[88px] text-neutral-500 text-[20px] font-normal leading-loose">
-                Talking to your customers should be easy.
-                <br />
-                Chat with them from the collaboration
-                <br />
-                tools you already use: Teams and Slack.
-              </div>
-            </div>
-          </div>
-          <div>
-            <img
-              className="w-[600px] h-[383px] rounded-md shadow mr-20 mt-10"
-              src="/iMAGE.png"
-            />
-            <img
-              className="w-64 h-[393px] left-[549px] top-[206px] absolute rounded-md"
-              src="/chatscreen.png"
-            />
-          </div>
+        <div className="hidden md:flex md:flex-1.5 flex-row pl-5 relative justify-end items-end">
+          <img
+            className="scale-80 top-0 right-0"
+            src="/iMAGE.png"
+            alt="screenshot"
+          />
+          <img
+            className="absolute scale-85 w-100 left-20 top-[150px]"
+            src="/chatscreen.png"
+            alt=""
+          />
         </div>
       </div>
-      <div>
-        <div className="w-[179.55px] h-[60px] pl-8 py-4 bg-gradient-to-r from-blue-600 to-violet-700 rounded-full shadow justify-start items-start ">
-          <div className="text-white text-[18px] font-medium leading-7">
-            Try for Free →
+      <div className="h-[200px]"></div>
+      <div className="flex flex-row">
+        <div className="flex-1">
+          <div className="w-full text-indigo-600 text-sm font-semibold leading-7">
+            Build revenue
           </div>
+          <div className="w-full h-[42px] text-zinc-900 text-4xl font-semibold leading-10">
+            Close More Sales Online
+          </div>
+          <div className="w-full h-[53px] text-neutral-500 text-[18px] font-normal leading-loose">
+            Engage potential customers when they need your help, close
+            <br />
+            more deals, and increase online revenue.
+          </div>
+          <div className="ml-10 mt-8">
+            <div className="w-full mt-4 h-[19px] left-[36.01px] top-[204px] text-gray-900 text-[16px] font-semibold leading-7">
+              Answer questions in real-time.{" "}
+            </div>
+            <span className="w-full left-[259.94px] top-[200px] text-neutral-500 text-[16px] font-normal leading-7">
+              Connect with customers when they{" "}
+            </span>
+            <span className="w-full left-[36.01px] top-[228px] text-neutral-500 text-[16px] font-normal leading-7">
+              are about to buy.
+            </span>
+            <div className="w-full h-[19px] left-[36.01px] top-[292px] text-gray-900 text-[16px] font-semibold leading-7">
+              Capture leads.{" "}
+            </div>
+            <span className="w-full left-[144.11px] top-[288px] text-neutral-500 text-[16px] font-normal leading-7">
+              Automate your lead capture and push transcripts
+            </span>
+            <span className="w-full left-[36.01px] top-[316px] text-neutral-500 text-[16px] font-normal leading-7">
+              and leads right into your CRM, ticketing, or marketing systems.
+            </span>
+            <div className="w-full h-[19px] left-[36.01px] top-[380px] text-gray-900 text-[16px] font-semibold leading-7">
+              Automate your sales process.{" "}
+            </div>
+            <span className="w-full left-[253.72px] top-[376px] text-gray-600 text-[16px] font-normal leading-7">
+              Pre-qualify visitors with initial{" "}
+            </span>
+            <span className="w-full left-[36.01px] top-[404px] text-neutral-500 text-[16px] font-normal leading-7">
+              questions, automate via chatbots, and escalate to your agents when
+              it makes sense.
+            </span>
+          </div>
+        </div>
+        <div className="hidden md:flex md:flex-1">
+          <img src="/landing2.png" alt="screenshot" />
         </div>
       </div>
     </div>
---
+++ src/components/bg-vector.tsx
@@ -0,0 +1,54 @@
+export default function BgVector() {
+  return (
+    <svg
+      className="absolute hidden md:flex top-0"
+      width="1080"
+      height="335"
+      viewBox="0 0 1080 335"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g filter="url(#filter0_f_1_630)">
+        <path
+          d="M184.735 224.152L229.27 274.347L60 198.7L323.404 224.152L492.94 133.751C494.025 175.522 530.375 243.171 667.092 179.597C837.99 100.13 792.417 30.5959 916.113 73.3862C1015.07 107.618 1024.62 166.608 1017.03 191.823L769.953 154.718L787.535 268.998L323.404 161.023L184.735 224.152Z"
+          fill="url(#paint0_linear_1_630)"
+          fill-opacity="0.2"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_f_1_630"
+          x="0"
+          y="0.340454"
+          width="1079.06"
+          height="334.007"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="BackgroundImageFix"
+            result="shape"
+          />
+          <feGaussianBlur
+            stdDeviation="30"
+            result="effect1_foregroundBlur_1_630"
+          />
+        </filter>
+        <linearGradient
+          id="paint0_linear_1_630"
+          x1="1019.47"
+          y1="60.3962"
+          x2="438.359"
+          y2="648.32"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#0052FF" />
+          <stop offset="1" stop-opacity="0.07" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
