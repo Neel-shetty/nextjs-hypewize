e1d1f8642d3a804b90ccdc34da47750d69248e5d
Neel <neelnarayanshetty@gmail.com>
2023-07-01 08:37:12 +0530

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,66 @@
-import Image from "next/image";
+import { MoveRight } from "lucide-react";
 
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <div className="bg-gray-50 ">
+      <div className="w-[90%] items-center flex flex-col justify-between ">
+        <div className="flex flex-row justify-between items-center p-5">
+          <div className="flex flex-row gap-5 justify-between w-full">
+            <div></div>
+            <div className="flex flex-row gap-5 justify-between">
+              <button className="border-[#2745FA] text-[#2745FA] px-5 py-2 border rounded-full">
+                Pricing
+              </button>
+
+              <button className="bg-[#2745FA] text-white px-5 py-2 rounded-full flex flex-row justify-center items-center gap-3">
+                Login
+                <MoveRight />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-row justify-between items-center">
+          <div className="flex-1">
+            {/* <p>GPT-Powered Live Chat</p>
+          <p>Your Gateway to Productive Communication</p> */}
+            <div className="w-[593px] h-44">
+              <span className="text-blue-600 text-[46px] font-medium leading-10 tracking-widest">
+                GPT-Powered Live Chat
+              </span>
+              <span className="text-zinc-900 text-[46px] font-medium leading-10 tracking-widest">
+                {" "}
+              </span>
+              <span className="text-black text-[42px] font-medium leading-10 tracking-widest">
+                Your Gateway to Productive Communication
+              </span>
+              <div className="w-[372.06px] h-[88px] text-neutral-500 text-[20px] font-normal leading-loose">
+                Talking to your customers should be easy.
+                <br />
+                Chat with them from the collaboration
+                <br />
+                tools you already use: Teams and Slack.
+              </div>
+            </div>
+          </div>
+          <div>
+            <img
+              className="w-[600px] h-[383px] rounded-md shadow mr-20 mt-10"
+              src="/iMAGE.png"
+            />
+            <img
+              className="w-64 h-[393px] left-[549px] top-[206px] absolute rounded-md"
+              src="/chatscreen.png"
+            />
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="w-[179.55px] h-[60px] pl-8 py-4 bg-gradient-to-r from-blue-600 to-violet-700 rounded-full shadow justify-start items-start ">
+          <div className="text-white text-[18px] font-medium leading-7">
+            Try for Free →
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/app/x-page.tsx
@@ -0,0 +1,220 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-gray-50">
+      <div className="w-[1198px] left-[273px] top-[1155px] absolute">
+        <img
+          className="w-[576px] h-[443px] left-[622px] top-[9px] absolute rounded-tl-xl shadow"
+          src=""
+        />
+        <div className="w-[510px] h-[460px] left-0 top-0 absolute">
+          <div className="w-[98.30px] h-[19px] left-0 top-0 absolute text-indigo-600 text-[16px] font-semibold leading-7">
+            Build revenue
+          </div>
+          <div className="w-[375.32px] h-[42px] left-0 top-[31px] absolute text-zinc-900 text-[36px] font-semibold leading-10">
+            Close More Sales Online
+          </div>
+          <div className="w-[482.22px] h-[53px] left-0 top-[101px] absolute text-neutral-500 text-[18px] font-normal leading-loose">
+            Engage potential customers when they need your help, close
+            <br />
+            more deals, and increase online revenue.
+          </div>
+          <div className="w-[224.13px] h-[19px] left-[36.01px] top-[204px] absolute text-gray-900 text-[16px] font-semibold leading-7">
+            Answer questions in real-time.{" "}
+          </div>
+          <div className="w-[250.06px] left-[259.94px] top-[200px] absolute text-neutral-500 text-[16px] font-normal leading-7">
+            Connect with customers when they
+          </div>
+          <div className="w-[119.03px] left-[36.01px] top-[228px] absolute text-neutral-500 text-[16px] font-normal leading-7">
+            are about to buy.
+          </div>
+          <div className="w-[108.31px] h-[19px] left-[36.01px] top-[292px] absolute text-gray-900 text-[16px] font-semibold leading-7">
+            Capture leads.{" "}
+          </div>
+          <div className="w-[347.08px] left-[144.11px] top-[288px] absolute text-neutral-500 text-[16px] font-normal leading-7">
+            Automate your lead capture and push transcripts
+          </div>
+          <div className="w-[443.10px] left-[36.01px] top-[316px] absolute text-neutral-500 text-[16px] font-normal leading-7">
+            and leads right into your CRM, ticketing, or marketing systems.
+          </div>
+          <div className="w-[217.91px] h-[19px] left-[36.01px] top-[380px] absolute text-gray-900 text-[16px] font-semibold leading-7">
+            Automate your sales process.{" "}
+          </div>
+          <div className="w-[208.05px] left-[253.72px] top-[376px] absolute text-gray-600 text-[16px] font-normal leading-7">
+            Pre-qualify visitors with initial
+          </div>
+          <div className="w-[440.10px] left-[36.01px] top-[404px] absolute text-neutral-500 text-[16px] font-normal leading-7">
+            questions, automate via chatbots, and escalate to your agents
+            <br />
+            when it makes sense.
+          </div>
+        </div>
+      </div>
+      <div className="w-[181px] pl-4 pr-[467px] py-4 left-[544px] top-[-1137px] absolute rounded-3xl border border border border border-blue-600 justify-start items-start inline-flex">
+        <div className="text-[60px] font-bold leading-10">Sales</div>
+      </div>
+      <div className="w-[280px] h-[46px] left-[1483px] top-[115px] absolute">
+        <div className="w-[105px] h-[46px] left-0 top-0 absolute">
+          <div className="left-[30px] top-[9px] absolute text-blue-600 text-[18px] font-normal leading-7">
+            Pricing
+          </div>
+          <div className="w-[105px] h-[46px] left-0 top-0 absolute rounded-[50px] border border border border border-neutral-200" />
+        </div>
+        <div className="w-[157px] h-[46px] left-[123px] top-0 absolute">
+          <div className="w-[157px] h-[46px] left-0 top-0 absolute bg-blue-600 rounded-[50px] border border border border border-neutral-200" />
+          <div className="left-[49px] top-[9px] absolute text-white text-[18px] font-normal leading-7">
+            Login →
+          </div>
+        </div>
+      </div>
+      <div className="w-[1538px] h-[599px] left-[233px] top-[315px] absolute">
+        <div className="w-[593px] h-[496px] left-0 top-[28px] absolute">
+          <div className="w-[593px] h-44 left-0 top-0 absolute">
+            <span className="text-blue-600 text-[46px] font-medium leading-10 tracking-widest">
+              GPT-Powered Live Chat
+            </span>
+            <span className="text-zinc-900 text-[46px] font-medium leading-10 tracking-widest">
+              {" "}
+            </span>
+            <span className="text-black text-[42px] font-medium leading-10 tracking-widest">
+              Your Gateway to Productive Communication
+            </span>
+          </div>
+          <div className="w-[372.06px] h-[88px] left-[3px] top-[176px] absolute text-neutral-500 text-[20px] font-normal leading-loose">
+            Talking to your customers should be easy.
+            <br />
+            Chat with them from the collaboration
+            <br />
+            tools you already use: Teams and Slack.
+          </div>
+          <div className="w-[375.43px] h-[88px] left-[3px] top-[296px] absolute text-neutral-500 text-[20px] font-normal leading-loose">
+            Build ChatGPT powered chatbots trained
+            <br />
+            on your website data. Easily handoff chats
+            <br />
+            between ChatGPT and human agents.
+          </div>
+          <div className="w-[179.55px] h-[60px] pl-8 pr-[31.55px] py-4 left-[3px] top-[436px] absolute bg-gradient-to-r from-blue-600 to-violet-700 rounded-full shadow justify-start items-start inline-flex">
+            <div className="text-white text-[18px] font-medium leading-7">
+              Try for Free →
+            </div>
+          </div>
+          <div className="w-[155.73px] h-[60px] pl-8 pr-[31.73px] py-4 left-[204.55px] top-[436px] absolute bg-white bg-opacity-0 rounded-full shadow shadow-inner justify-start items-start inline-flex">
+            <div className="text-indigo-600 text-[18px] font-medium leading-7">
+              View Demo
+            </div>
+          </div>
+        </div>
+        <img
+          className="w-[805px] h-[513px] left-[733px] top-0 absolute rounded-md shadow"
+          src="/iMAGE.png"
+        />
+        <img
+          className="w-64 h-[393px] left-[549px] top-[206px] absolute"
+          src="https://via.placeholder.com/256x393"
+        />
+      </div>
+      <div className="w-[1227px] left-[297px] top-[1867px] absolute">
+        <div className="w-[495px] h-[325px] left-0 top-[20px] absolute">
+          <div className="w-[46.30px] h-[19px] left-0 top-0 absolute text-blue-600 text-[16px] font-semibold leading-7">
+            Step 2
+          </div>
+          <div className="w-[435.92px] h-[82px] left-0 top-[31px] absolute text-gray-900 text-[36px] font-semibold leading-10">
+            Embed Live Chat Snippet On
+            <br />
+            Your Website
+          </div>
+          <div className="w-[495px] h-[85px] left-0 top-[141px] absolute text-neutral-500 text-[18px] font-normal leading-loose">
+            First, make sure that the live chat widget is visible on your
+            website. This is what customers will use to talk to you.
+          </div>
+          <div className="w-[491.14px] h-[67px] left-0 top-[258px] absolute text-neutral-500 text-[16px] font-normal leading-normal">
+            To make it appear, either insert the live chat code snippet into
+            your web page or use special plugins provided by Social Intents for
+            platforms like Shopify, WordPress, Bigcommerce, or Wix.
+          </div>
+        </div>
+        <img
+          className="w-[672px] h-[365px] left-[555px] top-0 absolute"
+          src="https://via.placeholder.com/672x365"
+        />
+      </div>
+      <div className="h-[810px] left-[297px] top-[2496px] absolute">
+        <div className="left-[315px] top-0 absolute text-zinc-900 text-[48px] font-semibold capitalize leading-10">
+          frequently asked questions.
+        </div>
+        <div className="w-[1216px] h-[708px] left-0 top-[102px] absolute">
+          <div className="w-[430.37px] h-[177px] left-0 top-0 absolute">
+            <div className="w-[430.37px] h-[57px] left-0 top-0 absolute text-zinc-900 text-[40px] font-semibold leading-10">
+              Have any Questions?
+            </div>
+            <div className="w-[378.10px] h-[19px] left-0 top-[69px] absolute text-neutral-500 text-[16px] font-normal leading-7">
+              Cant find the answer youre looking for? Chat with us.
+            </div>
+            <div className="w-[189.77px] h-[60px] pl-[31px] pr-[32.77px] py-4 left-0 top-[117px] absolute bg-blue-600 rounded-full shadow justify-start items-start inline-flex">
+              <div className="text-center text-white text-[18px] font-normal leading-7">
+                Chat With Us →
+              </div>
+            </div>
+          </div>
+          <div className="w-[696px] h-[708px] left-[520px] top-0 absolute">
+            <div className="w-[696px] h-[168px] p-6 left-0 top-0 absolute bg-white bg-opacity-0 rounded-2xl shadow flex-col justify-center items-start gap-2 inline-flex">
+              <div className="w-[337.28px] h-7 text-zinc-900 text-[24px] font-semibold leading-7">
+                Who should use Social Intents?
+              </div>
+              <div className="self-stretch h-[84px] pr-[25.52px] py-0.5 justify-start items-center inline-flex">
+                <div className="w-[622.48px] h-20 text-neutral-500 text-[20px] font-normal leading-7">
+                  Small businesses, entrepreneurs, marketers, and anyone who
+                  wants to
+                  <br />
+                  improve their sales. Especially teams that use Microsoft
+                  Teams, Slack,
+                  <br />
+                  Zoom, or Webex.
+                </div>
+              </div>
+            </div>
+            <div className="w-[696px] h-[168px] p-6 left-0 top-[208px] absolute bg-white bg-opacity-0 rounded-2xl shadow flex-col justify-center items-start gap-2 inline-flex">
+              <div className="w-[219.33px] h-7 text-zinc-900 text-[24px] font-semibold leading-7">
+                Is there a Free Trial?
+              </div>
+              <div className="self-stretch h-[84px] pr-[21.51px] py-0.5 justify-start items-center inline-flex">
+                <div className="w-[626.49px] h-20 text-neutral-500 text-[20px] font-normal leading-7">
+                  Yes! We offer a 14-Day free pro trial with all the bells and
+                  whistles to
+                  <br />
+                  make sure you love the product. Cancel at any time and there
+                  are never
+                  <br />
+                  any contracts.
+                </div>
+              </div>
+            </div>
+            <div className="w-[696px] h-[140px] p-6 left-0 top-[416px] absolute bg-white bg-opacity-0 rounded-2xl shadow flex-col justify-center items-start gap-2 inline-flex">
+              <div className="w-[437.48px] h-7 text-zinc-900 text-[24px] font-semibold leading-7">
+                Can I use live chat on multiple websites?
+              </div>
+              <div className="self-stretch h-14 pr-[2.36px] py-0.5 justify-center items-center inline-flex">
+                <div className="w-[645.64px] h-[52px] text-neutral-500 text-[20px] font-normal leading-7">
+                  Yes - absolutely! Each of our plans supports a tiered number
+                  of websites.
+                  <br />
+                  So just pick a plan that fits your needs.
+                </div>
+              </div>
+            </div>
+            <div className="w-[696px] h-28 pl-6 pr-[152.19px] pt-6 pb-[26px] left-0 top-[596px] absolute bg-white bg-opacity-0 rounded-2xl shadow flex-col justify-center items-start gap-2.5 inline-flex">
+              <div className="w-[304.81px] h-7 text-zinc-900 text-[24px] font-semibold leading-7">
+                Do I have to sign a contract?
+              </div>
+              <div className="w-[519.81px] h-6 text-neutral-500 text-[20px] font-normal leading-7">
+                No! There are no contracts and you can cancel at any time.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
