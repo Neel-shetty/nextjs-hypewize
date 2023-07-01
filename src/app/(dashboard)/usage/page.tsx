69698b4dbcb3305baed3f5ef93455f82a16ccda0
Neel <neelnarayanshetty@gmail.com>
2023-07-01 06:00:53 +0530

    usage screen

--- src/app/(dashboard)/usage/page.tsx
+++ src/app/(dashboard)/usage/page.tsx
@@ -5,8 +5,19 @@ interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
   return (
-    <div>
-      <UsageCard title={'1500'} description={'Total Messages'}/>
+    <div className="h-screen mx-6 mt-3">
+      <p className="mb-2 ml-1">Total</p>
+      <div className="flex flex-row gap-5">
+        <UsageCard title={"1500"} description={"Total Messages"} />
+        <UsageCard title={"358"} description={"Total Projects"} />
+        <UsageCard title={"15"} description={"Total Content "} />
+      </div>
+      <p className="mb-2 ml-1 mt-5">For This Month</p>
+      <div className="flex flex-row gap-5">
+        <UsageCard title={"1500"} description={"Total Messages"} />
+        <UsageCard title={"358"} description={"Total Projects"} />
+        <UsageCard title={"15"} description={"Total Content "} />
+      </div>
     </div>
   );
 };
--- src/components/usage-card.tsx
+++ src/components/usage-card.tsx
@@ -1,13 +1,21 @@
 import { Card } from "./ui/card";
+import { Mail } from "lucide-react";
 
 export default function UsageCard({
   title,
   description,
+  icon,
 }: {
   title: string;
   description: string;
 }) {
-  return <Card>
-    
-  </Card>
+  return (
+    <Card className="flex flex-col items-center justify-center w-[240px] p-5">
+      <div className="w-[49px] h-[49px] opacity-20 bg-blue-600 bg-opacity-40 rounded-full ">
+        {/* <Mail className="w-[40px] h-[40px] text-[#2745FA] z-10" /> */}
+      </div>
+      <div className="font-bold text-lg">{title}</div>
+      <div>{description}</div>
+    </Card>
+  );
 }
