5633256b941e2f29f638164f3c4c3cdd3bdb674c
Neel <neelnarayanshetty@gmail.com>
2023-07-02 02:31:04 +0530

    chat

--- src/app/(dashboard)/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/chat/[chatId]/page.tsx
@@ -1,29 +1,34 @@
-'use client'
-// import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
-import {
-  MainContainer,
-  ChatContainer,
-  MessageList,
-  Message,
-  MessageInput,
-} from "@chatscope/chat-ui-kit-react";
+import Message from "@/components/message";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Send } from "lucide-react";
 
 export default function Chat() {
-  <div className={'relative h-[500px]'}>
-    <MainContainer>
-      <ChatContainer>
-        <MessageList>
-          <Message
-          //@ts-ignore
-            model={{
-              message: "Hello my friend",
-              sentTime: "just now",
-              sender: "Joe",
-            }}
-          />
-        </MessageList>
-        <MessageInput placeholder="Type message here" />
-      </ChatContainer>
-    </MainContainer>
-  </div>;
+  return (
+    <div className="mx-6 mt-3 bg-[#F7F8FA] pb-10 ">
+      <div className="h-[600px] flex flex-col">
+        <div className="h-[100px] bg-[#2745FA] rounded-t-lg flex flex-col justify-center items-center">
+          <p className="text-white font-bold text-xl">Name</p>
+          <p className="text-white font-medium">AI Assistant</p>
+        </div>
+        <div className="flex-1 bg-white overflow-y-scroll">
+          {[1, 2, 3, 4, 5, 6, 7, 8].map((item) => (
+            <Message
+              key={item}
+              align={item % 2 === 0 ? "right" : "left"}
+              name="Name"
+              time="12:00AM"
+              message="We work to make sure your business is available when your customers want to shop.  available when your customers want to shop."
+            />
+          ))}
+        </div>
+        <div className="align-bottom gap-5 flex flex-row">
+          <Input width={""} />
+          <Button className="bg-[#2745FA]">
+            <Send />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
